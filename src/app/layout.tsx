e1ab8c0d34522cc7d855710c696ef7c7c6a2bf93
elguiriashing <alexashing1@gmail.com>
2026-09-22 01:41:15 +0200

    Load Google Analytics tag before interaction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,9 +54,9 @@ export default async function RootLayout({
       <body>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PW8547QDGD"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="beforeInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
